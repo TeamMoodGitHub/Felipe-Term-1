7ecbf868af982f183682a4a3bce4c2ff610aaf28
Felipe Linares <flinaresrm@gmail.com>
2017-09-13 16:32:06 -0400

    Page now refreshes every 15 seconds to determine whether or not player is in game

--- testRank/routes/rank.js
+++ testRank/routes/rank.js
@@ -12,17 +12,30 @@ var host = 'https://na1.api.riotgames.com/lol/';
 var getIdUrl = 'summoner/v3/summoners/by-name/';
 var getMatchListUrl = 'match/v3/matchlists/by-account/';
 var getRankLpUrl = 'league/v3/leagues/by-summoner/';
-var apiKey = '?api_key=RGAPI-6937353f-e3b0-486c-9017-92dc9b89e978';
+var getSpectator = '/spectator/v3/active-games/by-summoner/';
+var apiKey = '?api_key=RGAPI-febd9bd2-5b3d-48ec-80e7-c965f13bcaf4';
 
-
+var summonerId;
 var matchInfo;
 
+var getCurrentGame = function(summonerId, callback){
+  request(host + getSpectator + summonerId + apiKey, function(error, response, body){
+    console.log(summonerId);
+    if(!error && response.statusCode == 200){
+      var inGame = true;
+      if(JSON.parse(body) == null){
+        inGame = false;
+      }
+    }
+
+    callback(null, summonerId, inGame);
+  })
+}
+
 var getMatchList = function(summonerId, callback) {
-  console.log(host + getMatchListUrl + summonerId + apiKey);
   request(host + getMatchListUrl + summonerId + apiKey, function(error, response, body){
     if(!error && response.statusCode == 200){
       var matchInfo = JSON.parse(body);
-      console.log(matchInfo);
     }
     else{
       console.log(error);
@@ -30,7 +43,7 @@ var getMatchList = function(summonerId, callback) {
   });
 }
 
-var getRankLp = function(summonerId, callback){
+var getRankLp = function(summonerId, inGame, callback){
   request(host + getRankLpUrl + summonerId + apiKey, function(error, response, body){
     if(!error && response.statusCode == 200){
       var playerInfo = JSON.parse(body);
@@ -51,7 +64,7 @@ var getRankLp = function(summonerId, callback){
       });
       var rank = player[0].rank;
       var lp = player[0].leaguePoints;
-      callback(null,rank,tier,lp);
+      callback(null,rank,tier,lp,inGame);
     }
     else{
       console.log(error);
@@ -64,6 +77,7 @@ var getSummonerId = function(summonerName, callback){
       if(!error && response.statusCode == 200){
         info = JSON.parse(body);
         console.log(info.id);
+        summonerId = info.id;
         callback(null,info.id);
       }
       else{
@@ -81,9 +95,17 @@ router.post('/', (req, res, next) => {
       callback(null, user);
     },
     getSummonerId,
+    getCurrentGame,
     getRankLp,
-    function(rank,tier,lp,callback){
-      res.render('rank',{title: 'Rank', rank : rank, tier : tier, lp: lp});
+    function(rank,tier,lp, inGame,callback){
+      var currentGame = "";
+      if(inGame){
+        currentGame = "Currently in Game";
+      }
+      else{
+        currentGame = "Not in game";
+      }
+      res.render('rank',{title: 'Rank', inGame : currentGame, rank : rank, tier : tier, lp: lp});
       callback(null, 'done');
     }
   ],
@@ -93,3 +115,31 @@ router.post('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+router.get('/', (req,res,next) => {
+  var user = summonerId;
+  console.log(user);
+  async.waterfall([
+    function(callback){
+      callback(null,user);
+    },
+    getCurrentGame,
+    function(summonerId, inGame,callback){
+    //  var status= "";
+      if(inGame){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ a: "Currently in game" }));
+      }
+      else{
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ a: "Not in game" }));
+      }
+    //  console.log(status);
+    //  res.setHeader('content-type', 'application/json');
+    //  res.send(status);
+    }
+  ],
+  function(err, string){
+    console.log(string);
+  });
+});
