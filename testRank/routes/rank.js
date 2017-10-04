6c56dfa36102ef14dfbf0f3088ab568dab52e8bb
Felipe Linares <flinaresrm@gmail.com>
2017-10-04 16:02:00 -0400

    rewrote methods to use firebase rather than many different params

--- testRank/routes/rank.js
+++ testRank/routes/rank.js
@@ -16,6 +16,7 @@ var firebase = require('firebase').initializeApp({
 var ref = firebase.database().ref().child('node-client');
 var logsRef = ref.child('logs');
 var messagesRef = ref.child('messages');
+var usersRef = ref.child('users');
 //var messageRef = messagesRef.push(message);
 
 var host = 'https://na1.api.riotgames.com/lol/';
@@ -24,36 +25,40 @@ var getMatchListUrl = 'match/v3/matchlists/by-account/';
 var getMatchesUrl = 'match/v3/matches/'
 var getRankLpUrl = 'league/v3/leagues/by-summoner/';
 var getSpectator = '/spectator/v3/active-games/by-summoner/';
-var apiKey = '?api_key=RGAPI-eae074dc-6df2-4306-b8e4-4780c6a8e38f';
+var apiKey = '?api_key=RGAPI-e8226e0a-143a-4e08-9d2c-220c81f4014d';
 
 var matchInfo;
 
-var getCurrentGame = function(matches, summonerId, callback){
-  request(host + getSpectator + summonerId + apiKey, function(error, response, body){
-    if(!error && response.statusCode == 200){
-      var inGame = true;
-      if(JSON.parse(body) == null){
-        inGame = false;
+var getSummonerId = function(user, callback){
+  request(host + getIdUrl + user + apiKey, function(error, response, body){
+      if(!error && response.statusCode == 200){
+        info = JSON.parse(body);
+        summonerId = info.id;
+
+        var playerRef = usersRef.child(user);
+
+        playerRef.set({
+          summonerId: summonerId,
+          accountId: info.accountId
+        });
+        callback(null, user);
       }
-    }
-    callback(null, summonerId, inGame, matches);
-  })
+      else{
+        console.log(error);
+      }
+    });
 }
 
-var getCurrentGameRefresh = function(summonerId, callback){
-  request(host + getSpectator + summonerId + apiKey, function(error, response, body){
-    if(!error && response.statusCode == 200){
-      var inGame = true;
-      if(JSON.parse(body) == null){
-        inGame = false;
-      }
-    }
-    callback(null, summonerId, inGame);
-  })
-}
+var getMatchList = function(user, callback) {
+  var summonerId;
+  var accountId;
 
+  var thisRef = usersRef.child(user);
+  thisRef.on("value", function(snapshot){
+    summonerId = snapshot.val()["summonerId"];
+    accountId =  snapshot.val()["accountId"];
+    });
 
-var getMatchList = function(summonerId, accountId,callback) {
   request(host + getMatchListUrl + accountId + "/recent" + apiKey, function(error, response, body){
     if(!error && response.statusCode == 200){
       var matchInfo = JSON.parse(body);
@@ -64,15 +69,96 @@ var getMatchList = function(summonerId, accountId,callback) {
         if((today - matches[i]["timestamp"]) / 86400000 <= 7)
           recentMatches.push(matches[i]["gameId"]);
       }
+      thisRef.update({
+        matchIds: recentMatches
+      });
     }
     else{
       console.log(error);
     }
-    callback(null, summonerId, recentMatches);
+    callback(null, user);
   });
 }
 
-var getRankLp = function(summonerId, inGame, matches,callback){
+var getMatches = function(user, callback){
+  var matches = [];
+  var summonerId;
+  var matchList;
+
+  var thisRef = usersRef.child(user);
+  thisRef.on("value", function(snapshot){
+    summonerId = snapshot.val()["summonerId"];
+    matchList = snapshot.val()["matchIds"];
+    });
+
+  for(i = 0; i < matchList.length; i++){
+
+    request(host + getMatchesUrl + matchList[i] + apiKey, function(error, response, body){
+      if(!error && response.statusCode == 200){
+        var info = JSON.parse(body);
+        var playerIdentity;
+
+        for(j = 0; j < info["participantIdentities"].length; j++){
+          if(info["participantIdentities"][j]["player"]["summonerId"] == summonerId){
+            playerIdentity = j;
+
+            var match = {
+              kills: info["participants"][j]["stats"]["kills"],
+              assists: info["participants"][j]["stats"]["assists"],
+              deaths: info["participants"][j]["stats"]["deaths"]
+            }
+            matches.push(match);
+            if(matches.length == matchList.length){
+              thisRef.update({
+                matches: matches
+              })
+              callback(null,user);
+            }
+          }
+        }
+      }
+      else{
+        console.log(error);
+      }
+    })
+  }
+
+}
+
+var getCurrentGame = function(user, callback){
+  var summonerId;
+
+  var thisRef = usersRef.child(user);
+  thisRef.on("value", function(snapshot){
+    summonerId = snapshot.val()["summonerId"];
+  });
+
+  console.log(host + getSpectator + summonerId + apiKey);
+  request(host + getSpectator + summonerId + apiKey, function(error, response, body){
+    console.log(response.statusCode);
+    if(!error && response.statusCode == 200){
+      console.log(body);
+      var inGame = true;
+      if(JSON.parse(body) == null){
+        inGame = false;
+      }
+      console.log(inGame);
+      thisRef.update({
+        gameStatus: inGame
+      })
+    }
+    callback(null, user);
+  })
+}
+
+var getRankLp = function(user, callback){
+  var summonerId;
+
+  var thisRef = usersRef.child(user);
+  thisRef.on("value", function(snapshot){
+    summonerId = snapshot.val()["summonerId"];
+    });
+
   request(host + getRankLpUrl + summonerId + apiKey, function(error, response, body){
     if(!error && response.statusCode == 200){
       var playerInfo = JSON.parse(body);
@@ -92,7 +178,12 @@ var getRankLp = function(summonerId, inGame, matches,callback){
       });
       var rank = player[0].rank;
       var lp = player[0].leaguePoints;
-      callback(null,rank,tier,lp,inGame, matches);
+      thisRef.update({
+        rank: rank,
+        tier: tier,
+        lp: lp
+      })
+      callback(null,user);
     }
     else{
       console.log(error);
@@ -100,56 +191,21 @@ var getRankLp = function(summonerId, inGame, matches,callback){
   });
 }
 
-
-var getMatches = function(summonerId, matchList, callback){
-  var matches = [];
-  for(i = 0; i < matchList.length; i++){
-    request(host + getMatchesUrl + matchList[i] + apiKey, function(error, response, body){
-      if(!error && response.statusCode == 200){
-        var info = JSON.parse(body);
-        var playerIdentity;
-
-        for(j = 0; j < 10; j++){
-          if(info["participantIdentities"][j]["player"]["summonerId"] == summonerId){
-            playerIdentity = j;
-
-            var match = {
-              kills: info["participants"][j]["stats"]["kills"],
-              assists: info["participants"][j]["stats"]["assists"],
-              deaths: info["participants"][j]["stats"]["deaths"]
-            }
-            matches.push(match);
-            if(matches.length == matchList.length){
-              callback(null, matches, summonerId);
-            }
-          }
-        }
+var getCurrentGameRefresh = function(summonerId, callback){
+  request(host + getSpectator + summonerId + apiKey, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var inGame = true;
+      if(JSON.parse(body) == null){
+        inGame = false;
       }
-      else{
-        console.log(error);
-      }
-    })
-  }
-
+    }
+    callback(null, summonerId, inGame);
+  })
 }
-
-var getSummonerId = function(summonerName, callback){
-  request(host + getIdUrl + summonerName + apiKey, function(error, response, body){
-      if(!error && response.statusCode == 200){
-        info = JSON.parse(body);
-        summonerId = info.id;
-        ref.push(summonerId);
-        callback(null,info.id,info.accountId);
-      }
-      else{
-        console.log(error);
-      }
-    });
-}
-
 
 router.post('/', (req, res, next) => {
   var user = req.body.name;
+
   async.waterfall([
     function(callback){
       callback(null, user);
@@ -159,7 +215,24 @@ router.post('/', (req, res, next) => {
     getMatches,
     getCurrentGame,
     getRankLp,
-    function(rank,tier,lp, inGame, matches, callback){
+    function(user, callback){
+      var summonerId;
+      var rank;
+      var tier;
+      var lp;
+      var inGame;
+      var matches;
+
+      var thisRef = usersRef.child(user);
+      thisRef.on("value", function(snapshot){
+        summonerId = snapshot.val()["summonerId"];
+        rank = snapshot.val()["rank"];
+        tier = snapshot.val()["tier"];
+        lp = snapshot.val()["lp"];
+        inGame = snapshot.val()["gameStatus"];
+        matches = snapshot.val()["matches"];
+        });
+
       var currentGame = "";
       if(inGame){
         currentGame = "Currently in Game";
@@ -199,6 +272,5 @@ router.post('/refresh', (req,res,next) => {
     console.log(string);
   });
 });
-
 
 module.exports = router;
